6170858d91b20d4c5e013243204bcc14d0f0995a
jinsu <croco0120@gmail.com>
2020-08-11 16:22:10 +0900

    db default change

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'linner',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'USER':'linneruser',
+        'PASSWORD':'1234',
     }
 }
 
