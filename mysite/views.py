95606086397fda2e4b5e120087e6328cc99611a8
wonjongah <wja1511@naver.com>
2020-08-18 16:09:56 +0900

    다시 올림

--- mysite/views.py
+++ mysite/views.py
@@ -5,10 +5,12 @@ from django.urls import reverse_lazy
 # 유저
 from django.contrib.auth.mixins import AccessMixin
 from django.views.defaults import permission_denied
-
+from .form import CreateUserForm
 
 # 순수하게 html 템플릿만 운영할 수 있도록 도와주는
 # 공통 사용할 것~~~~~~~~~~~~~~~~~
+
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -26,9 +28,19 @@ class OwnerOnlyMixin(AccessMixin):
     permission_denied_message = "Owner only can update/delete the object"
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()  # 모델 인스턴스 얻기
+        if self.request.user != self.object.Rec_conMemID:
+            self.handle_no_permission()
+        return super().get(request, *args, **kwargs)
+
+class OwnerOnlyMixin1(AccessMixin):
+    raise_exception = True
+    permission_denied_message = "Owner only can update/delete the object"
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()  # 모델 인스턴스 얻기
         if self.request.user != self.object.owner:
             self.handle_no_permission()
         return super().get(request, *args, **kwargs)
+
 
 class OwnerOnlyMixin2(AccessMixin):
     raise_exception = True
